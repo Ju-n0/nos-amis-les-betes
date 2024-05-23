f202cd3ebb27125c18fc575e5c349c4d333ce995
Ju-n0 <juliettenogueira.pro@gmail.com>
2024-05-23 20:47:47 +0200

    add: github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
 
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App.tsx";
 import "./index.scss";
